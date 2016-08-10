eea0ecc6a7d5bc4b2ef8f1adda022fb5a77d91a1
Mohammad Mohammadi <mohammadi@kpp.ir>
2016-08-10 12:55:41 +0430

    PUSH-10-08-2016
    change listener for ComboBox

--- src/controller/RootController.java
+++ src/controller/RootController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.SingleSelectionModel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,10 +15,10 @@ import java.util.ResourceBundle;
 public class RootController implements Initializable {
 
     @FXML
-    private ComboBox comboBox ;
+    private ComboBox<Choice> comboBox;
 
     @FXML
-    private void closeHandler(){
+    private void closeHandler() {
         System.exit(0);
     }
 
@@ -25,22 +28,52 @@ public class RootController implements Initializable {
         ObservableList<Choice> choices = FXCollections.observableArrayList();
         choices.add(new Choice(null, "No selection"));
         comboBox.setItems(choices);
+
+        comboBox.valueProperty().addListener(new ChangeListener<Choice>() {
+            @Override
+            public void changed(ObservableValue<? extends Choice> observable, Choice oldValue, Choice newValue) {
+                System.out.println();
+            }
+        });
+
+
     }
 
 
+
+
     class Choice {
-        Integer id; String displayString;
-        Choice(Integer id)                       { this(id, null); }
-        Choice(String  displayString)            { this(null, displayString); }
-        Choice(Integer id, String displayString) { this.id = id; this.displayString = displayString; }
-        @Override public String toString() { return displayString; }
-        @Override public boolean equals(Object o) {
+        Integer id;
+        String displayString;
+
+        Choice(Integer id) {
+            this(id, null);
+        }
+
+        Choice(String displayString) {
+            this(null, displayString);
+        }
+
+        Choice(Integer id, String displayString) {
+            this.id = id;
+            this.displayString = displayString;
+        }
+
+        @Override
+        public String toString() {
+            return displayString;
+        }
+
+        @Override
+        public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Choice choice = (Choice) o;
             return displayString != null && displayString.equals(choice.displayString) || id != null && id.equals(choice.id);
         }
-        @Override public int hashCode() {
+
+        @Override
+        public int hashCode() {
             int result = id != null ? id.hashCode() : 0;
             result = 31 * result + (displayString != null ? displayString.hashCode() : 0);
             return result;
