7e5069049e51566762d3d39b6953b8f804d41db3
Moolerian <Moolerian@Gmail.com>
2016-08-10 22:34:42 +0430

    PUSH-10-08-2016

--- src/controller/RootController.java
+++ src/controller/RootController.java
@@ -1,21 +1,33 @@
 package controller;
 
+import gov.nasa.worldwind.Model;
+import gov.nasa.worldwind.WorldWind;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.awt.WorldWindowGLJPanel;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.layout.BorderPane;
+import model.Shape;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
 
     @FXML
-    private ComboBox<Choice> comboBox;
+    private ComboBox<Shape> comboBox;
+    @FXML
+    private BorderPane borderPane;
+
+    private WorldWindowGLJPanel wwj;
 
     @FXML
     private void closeHandler() {
@@ -25,58 +37,43 @@ public class RootController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ObservableList<Choice> choices = FXCollections.observableArrayList();
-        choices.add(new Choice(null, "No selection"));
-        comboBox.setItems(choices);
+        Shape selectOneOption = new Shape();
+        selectOneOption.setId(null);
+        selectOneOption.setDisplayName("انتخاب کنید");
+        comboBox.setValue(selectOneOption);
 
-        comboBox.valueProperty().addListener(new ChangeListener<Choice>() {
+        //TODO fetch all other shapes from database and add in foreach to #comboBox
+        ObservableList<Shape> shapes = FXCollections.observableArrayList();
+
+        // TODO replace with lambda expression
+        comboBox.valueProperty().addListener(new ChangeListener<Shape>() {
             @Override
-            public void changed(ObservableValue<? extends Choice> observable, Choice oldValue, Choice newValue) {
-                System.out.println();
+            public void changed(ObservableValue<? extends Shape> observable, Shape oldValue, Shape newValue) {
+                System.out.println(newValue);
             }
         });
 
+        borderPane.setRight(buildWW());
 
     }
 
+    //TODO move it to util
+    private SwingNode buildWW() {
+        SwingNode node = new SwingNode();
 
+        SwingUtilities.invokeLater(() -> {
+            wwj = new WorldWindowGLJPanel();
+            Model m = (Model) WorldWind.createConfigurationComponent(AVKey.MODEL_CLASS_NAME);
+            wwj.setModel(m);
+            wwj.setBounds(0, 0, 600, 500);
+            JPanel panel = new JPanel();
+            panel.setLayout(null);
+            panel.setBounds(200, 100, 600, 500);
+            panel.add(wwj);
+            node.setContent(panel);
+            node.relocate(170, 10);
+        });
+        return node;
 
-
-    class Choice {
-        Integer id;
-        String displayString;
-
-        Choice(Integer id) {
-            this(id, null);
-        }
-
-        Choice(String displayString) {
-            this(null, displayString);
-        }
-
-        Choice(Integer id, String displayString) {
-            this.id = id;
-            this.displayString = displayString;
-        }
-
-        @Override
-        public String toString() {
-            return displayString;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Choice choice = (Choice) o;
-            return displayString != null && displayString.equals(choice.displayString) || id != null && id.equals(choice.id);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = id != null ? id.hashCode() : 0;
-            result = 31 * result + (displayString != null ? displayString.hashCode() : 0);
-            return result;
-        }
     }
 }
---
+++ src/model/Shape.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.io.Serializable;
+
+/**
+ * Created by mohammad on 8/10/2016.
+ */
+public class Shape implements Serializable {
+
+    private static final long serialVersionUID = -2440849599660810812L;
+
+    private Integer id;
+
+    private String displayName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shape shape = (Shape) o;
+        if (displayName != null ? !displayName.equals(shape.displayName) : shape.displayName != null) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (displayName != null ? displayName.hashCode() : 0);
+        return result;
+    }
+}
